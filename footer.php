a99982bc342da9ea7ed03c48c9c6f2aed4a11c8b
Shubhranshu Shekhar Behera <beherashubhranshu000@gmail.com>
2024-01-24 23:46:25 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="footer text-center bg-light">
+	© Copyright <span id="demo"></span> <span class="brand">Online Blood Bank System by GOVT.  </span> All Rights Reserved.
+	<script>
+		date = new Date();
+    	document.getElementById("demo").innerHTML=date.getFullYear();
+    </script>
+</footer>
